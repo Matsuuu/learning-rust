19b33b8d03f33948d4c9a4fbd15d1d718bb368bc
Matsuuu <huhta.matias@gmail.com>
2023-01-03 22:29:44 +0200

    Hello

---
+++ hello-world/src/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    println!("Hello, {}!", "World");
+    println!("Hello, {0}, and {1}!", "World", "Matsu");
+
+    println!("{greeting} {name}!",
+        greeting = "Hi",
+        name = "Matsu"
+    );
+
+
+}
